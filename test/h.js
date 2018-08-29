a77831cf652d4b5265c8d57e3ecdf7d57e30987a
Vinnie Pepi <vinnie@factual.com>
2018-08-29 10:31:03 +0800

    adds page parsing class

--- test/h.js
+++ test/h.js
@@ -1,8 +1,21 @@
-
 const fs = require('fs');
 const path = require('path');
 const fileName = './btext.txt';
 const cheerio = require('cheerio');
 
 const text = fs.readFileSync(path.resolve(__dirname, fileName)).toString();
-exports.$ = cheerio.load(text);
+$ = cheerio.load(text);
+exports.$ = $;
+
+exports.iterate = (src, fn = console.log) => {
+  src.each((i, ele) => {
+    fn($(ele));
+  });
+};
+
+
+const PageParser = require('../lib/bball-ref-page-parser');
+
+exports.test = () => {
+  new PageParser(text);
+};
