4aadfacc2e5d162ae4a3a4271fa2918c89a918e4
Vinnie Pepi <vinnie@factual.com>
2018-09-01 10:30:39 +0800

    runs and saves bball ref

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 const bballRef = require('./lib/bball-ref');
-bballRef.run();
+const dateStr = new Date().toISOString().split('T')[0];
+
+//const OUT_DIR = `data_output/${dateStr}`;
+const OUT_PATH = path.resolve(__dirname, `data_output/${dateStr}`);
+
+const writeTo = (filename) => {
+  return (content) => {
+    if (typeof content !== 'string') {
+      content = JSON.stringify(content);
+    }
+    fs.writeFile(path.join(OUT_PATH, filename), content, (err) => {
+      if (err) throw err;
+      console.log(`data written to ${filename}`);
+    });
+  };
+};
+
+fs.mkdir(OUT_PATH, (err) => {
+  bballRef.run(writeTo('bball-ref.json'));
+});
